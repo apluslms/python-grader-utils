b4e06eff508b314e5afeb44b0aa724293a6f6c02
Matias Lindgren <matias.lindgren@aalto.fi>
2017-08-11 14:02:56 +0300

    restructure into package

---
+++ setup.py
@@ -0,0 +1,64 @@
+"""A setuptools based setup module.
+
+See:
+https://packaging.python.org/en/latest/distributing.html
+https://github.com/pypa/sampleproject
+"""
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='graderutils',
+
+    # Versions should comply with PEP440.  For a discussion on single-sourcing
+    # the version across setup.py and the project code, see
+    # https://packaging.python.org/en/latest/single_source_version.html
+    version='1.0.0',
+
+    description='Utilities for Python based grader test pipelines',
+    long_description=long_description,
+
+    # The project's main homepage.
+    # url='https://github.com/todo'
+
+    # Author details
+    author='Matias Lindgren',
+    author_email='matias.lindgren@gmail.com',
+
+    license='MIT',
+
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+
+        'License :: OSI Approved :: MIT License',
+
+        'Programming Language :: Python :: 3',
+    ],
+
+    packages=find_packages(),
+
+    include_package_data=True,
+
+    install_requires=[
+        'Jinja2',
+    ],
+
+)

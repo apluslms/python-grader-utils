2202bb1b80e9310ac8ad905670d3cd8b81fda03e
Matias Lindgren <matias.lindgren@aalto.fi>
2017-08-11 14:05:41 +0300

    add url

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
     long_description=long_description,
 
     # The project's main homepage.
-    # url='https://github.com/todo'
+    url='https://github.com/matiaslindgren/grader-utils',
 
     # Author details
     author='Matias Lindgren',

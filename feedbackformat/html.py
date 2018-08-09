98bfba50c3aaabe707b49c1a14bc031adeb68182
Matias Lindgren <matias.lindgren@aalto.fi>
2018-08-09 16:40:48 +0300

    Add feature for generating output into a full HTML5 document

--- feedbackformat/html.py
+++ feedbackformat/html.py
@@ -25,7 +25,7 @@ def _load_package_template(name):
     return _load_template(package_loader, name)
 
 
-def grading_data_to_html(grading_data, custom_template_path):
+def grading_data_to_html(grading_data, custom_template_path, extends_base=False):
     """
     Format a "Grading feedback" JSON schema object as HTML.
     """
@@ -35,7 +35,7 @@ def grading_data_to_html(grading_data, custom_template_path):
         # Extend default template with given custom template
         grading_data["feedback_template"] = feedback_template
         feedback_template = _load_template_file(custom_template_path)
-    return feedback_template.render(**grading_data)
+    return feedback_template.render(**grading_data, extends_base=extends_base)
 
 
 if __name__ == "__main__":
@@ -51,7 +51,11 @@ if __name__ == "__main__":
     )
     parser.add_argument("--grader-container",
         action="store_true",
-        help="If given, print output to stderr and points to stdout"
+        help="Print generated output to stderr instead of stdout. Additionally, print points and max points to stdout."
+    )
+    parser.add_argument("--full-document",
+        action="store_true",
+        help="Embed results into a full HTML5 document."
     )
     args = parser.parse_args()
     grading_data = json.load(sys.stdin)
@@ -66,7 +70,7 @@ if __name__ == "__main__":
             print("Input does not conform to JSON schema 'Grading feedback'. Run with --verbose to show full validation error.")
             sys.exit(1)
     # Input is valid, render to html
-    html_feedback = grading_data_to_html(grading_data, args.custom_template)
+    html_feedback = grading_data_to_html(grading_data, args.custom_template, args.full_document)
     if args.grader_container:
         print(html_feedback, file=sys.stderr)
         print("TotalPoints: {}\nMaxPoints: {}".format(grading_data["points"], grading_data["maxPoints"]))

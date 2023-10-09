129d251e3373c8aa3d4ab9623de810a05e82d8e6
Jimmy Ihalainen <jimmy.ihalainen@gmail.com>
2023-10-09 09:24:16 +0300

    Release v4.9

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='graderutils',
-    version='4.8',
+    version='4.9',
     description='Utilities for Python based grader test pipelines',
     long_description=readmefile_contents,
 
